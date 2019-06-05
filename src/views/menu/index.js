faa77520d1cecc14360e83384d81da608ef15c77
aoxianghua <2878106303@qq.com>
2019-06-06 00:01:49 +0800

    左侧菜单

--- src/views/menu/index.js
+++ src/views/menu/index.js
@@ -13,17 +13,26 @@ class Menu extends Component {
   constructor(props) {
     super(props);
     this.data.menuList = getMenuList();
+    this.menuListClick = this.menuListClick.bind(this);
+    this.state = {
+      focusKey: null,
+    };
+  }
+
+  menuListClick = (list) => {
+    this.setState({ focusKey: list.key });
   }
 
   render() {
     const { menuList } = this.data;
+    const { focusKey } = this.state;
 
     return (
       <div className={styles.menu}>
         <ul>
           {
             menuList.map(list => (
-              <li>
+              <li onClick={() => this.menuListClick(list)} className={focusKey === list.key ? 'list-focus' : null}>
                 <Link to={list.router}>{list.title}</Link>
               </li>
             ))
